58872df00bd55c484aa8f0993f177c95cc9258cc
Giovane <giovanedaniell@gmail.com>
2024-01-15 19:14:26 -0300

    feat: add hubspot token to env file

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,6 +4,7 @@ interface Env {
   port: string
   awsBearerToken: string
   awsApiUrl: string
+  hubspotAccessToken: string
 }
 
 dotenv.config()
@@ -11,9 +12,11 @@ dotenv.config()
 const port = process.env.PORT as string
 const awsBearerToken = process.env.AWS_BEARER_TOKEN as string
 const awsApiUrl = process.env.AWS_API_URL as string
+const hubspotAccessToken = process.env.HUBSPOT_ACCESS_TOKEN as string
 
 export const env: Env = {
   port,
   awsApiUrl,
-  awsBearerToken
+  awsBearerToken,
+  hubspotAccessToken
 }
