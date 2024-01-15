e02d003d0e68d149aec5d65922ce4166a0967804
Giovane <giovanedaniell@gmail.com>
2024-01-15 19:14:53 -0300

    feat: add request to create batch contacts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import express from 'express'
 import { env } from './config/env'
 import { AwsGateway } from './gateways/aws'
+import { HubspotGateway } from './gateways/hubspot';
 
 const app = express()
 
 const awsGateway = new AwsGateway();
+const hubspotGateway = new HubspotGateway(awsGateway);
 
 app.get('/', async (_req, res) => {
-  const contacts = await awsGateway.getContacts();
+  const contacts = await hubspotGateway.createBatchContacts();
 
-  return res.status(200).json(contacts)
+  return res.send(200).json(contacts)
 })
 
 app.listen(env.port, () => {
