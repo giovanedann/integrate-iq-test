4ba165c72ab9ecceea16652ac3518e8ef42a12e9
Giovane <giovanedaniell@gmail.com>
2024-01-16 01:43:25 -0300

    feat: add endpoint to execute usecase method

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,16 @@ import express from 'express'
 import { env } from './config/env'
 import { AwsGateway } from './gateways/aws'
 import { HubspotGateway } from './gateways/hubspot';
+import { CreateManyHubspotContactsUseCase } from './usecases/hubspot/contacts/create-many/create-many-hubspot-contacts.usecase';
 
 const app = express()
 
 const awsGateway = new AwsGateway();
-const hubspotGateway = new HubspotGateway(awsGateway);
+const hubspotGateway = new HubspotGateway();
 
-app.get('/', async (_req, res) => {
-  const contacts = await hubspotGateway.createBatchContacts();
+const createManyHubspotContactsUsecase = new CreateManyHubspotContactsUseCase(awsGateway, hubspotGateway)
 
-  return res.send(200).json(contacts)
-})
+app.get('/contacts/create-many', createManyHubspotContactsUsecase.execute)
 
 app.listen(env.port, () => {
   console.log(`Server running on http://localhost:${env.port} 🚀`)
