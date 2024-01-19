bf6120dbf735ef5fe0e0a485ad559ba3751ae61b
Giovane <giovanedaniell@gmail.com>
2024-01-19 16:18:53 -0300

    refactor: change hubspot gateway batch creation handle

--- src/errors/ContactExistError.ts
+++
@@ -1,6 +0,0 @@
-export class ContactExistError extends Error {
-  constructor() {
-    super('You have tried to register a contact with an e-mail that already exists.')
-    this.name = 'ContactExistError'
-  }
-}
--- src/gateways/hubspot/index.ts
+++ src/gateways/hubspot/index.ts
@@ -1,23 +1,24 @@
-import { Client } from '@hubspot/api-client'
+import { Client } from '@hubspot/api-client';
 import { BatchInputSimplePublicObjectInputForCreate } from '@hubspot/api-client/lib/codegen/crm/contacts';
 
-import { env } from '../../config/env'
-import { IHubspotGateway } from '../../domain/gateways/hubspot/HubspotGateway.interface';
+import { env } from '../../config/env';
 import { AwsContact } from '../../domain/gateways/aws/AwsGateway.interface';
+import { IHubspotGateway } from '../../domain/gateways/hubspot/HubspotGateway.interface';
 import { chunkArray } from '../../helpers/chunkArray';
-import { ContactExistError } from '../../errors/ContactExistError';
 
 export class HubspotGateway implements IHubspotGateway {
   private readonly accessToken = env.hubspotAccessToken
   private readonly client = new Client({ accessToken: this.accessToken })
 
   async createBatchContacts(contacts: AwsContact[]) {
+    const chunkedArray = chunkArray(contacts, 100);
+    
     try {
-      const chunkedArray = chunkArray(contacts, 100);
-
       await Promise.all(chunkedArray.map((contacts) => {
-        const mappedContacts: BatchInputSimplePublicObjectInputForCreate = {
-          inputs: contacts.map((awsContact) => ({
+        const { contactsWithoutEmail } = this.filterUnregisteredContacts(contacts);
+
+        const mappedUnregisteredContacts: BatchInputSimplePublicObjectInputForCreate = {
+          inputs: contactsWithoutEmail.map((awsContact) => ({
             properties: {
               email: awsContact.email,
               firstname: awsContact.first_name,
@@ -30,10 +31,43 @@ export class HubspotGateway implements IHubspotGateway {
           }))
         }
 
-        return this.client.crm.contacts.batchApi.create(mappedContacts);
+        return this.client.crm.contacts.batchApi.create(mappedUnregisteredContacts);
       }))
     } catch (error) {
-      throw new ContactExistError();
+      // If this catches an error, means that the batch API throwed an error because some e-mail have already been registered
+      for (const contacts of chunkedArray) {
+        const { contactsWithEmail } = this.filterUnregisteredContacts(contacts)
+
+        await Promise.all(contactsWithEmail.map((contact) => this.upsertContact(contact)))
+      }
+    }
+  }
+
+  async upsertContact(contact: AwsContact) {
+    const body = {
+      properties: [
+        { property: 'firstname', value: contact.first_name },
+        { property: 'lastname', value: contact.last_name },
+        { property: 'website', value: 'https://integrateiq.com/' },
+        { property: 'company', value: 'Integrate IQ' },
+        { property: 'phone', value: contact.phone_number },
+      ]
+    }
+
+    await fetch(`https://api.hubapi.com/contacts/v1/contact/createOrUpdate/email/${contact.email}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body)
+    })
+  }
+
+  private filterUnregisteredContacts(contacts: AwsContact[]) {
+    const contactsWithEmail = contacts.filter(({ email }) => Boolean(email)) // checking if e-mail exists on the object
+    const contactsWithoutEmail = contacts.filter(({ email }) => !Boolean(email)) // checking if e-mail exists on the object
+
+    return {
+      contactsWithEmail,
+      contactsWithoutEmail
     }
   }
 }
